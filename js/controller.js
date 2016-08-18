015a7810dd027e1d0cf1e58860573c4c9711d60e
Liuxizi Xu <xlxzbisulyn@gmail.com>
2016-08-18 00:25:14 -0700

    ng repeat bug fixed

--- js/controller.js
+++ js/controller.js
@@ -10,28 +10,89 @@ app.controller('mainCtrl', function($scope, $firebaseObject) {
       monday: {
         name: 'Monday',
         slots: {
-          0900: {
+          900: {
             time: '9:00am',
             booked: false
           },
           0110: {
             time: '11:00am',
             booked: false
+          },
+          100: {
+            time: '1:00pm',
+            booked: false
+          },
+          300: {
+            time: '3:00pm',
+            booked: false
+          },
+          500: {
+            time: '5:00pm',
+            booked: false
+          },
+          700: {
+            time: '7:00pm',
+            booked: false
           }
-        }
+    	  }
       },
       tuesday: {
         name: 'Tuesday',
         slots: {
-          0900: {
+          900: {
             time: '9:00am',
             booked: false
           },
           0110: {
             time: '11:00am',
             booked: false
+          },
+          100: {
+            time: '1:00pm',
+            booked: false
+          },
+          300: {
+            time: '3:00pm',
+            booked: false
+          },
+          500: {
+            time: '5:00pm',
+            booked: false
+          },
+          700: {
+            time: '7:00pm',
+            booked: false
           }
-        }
+    	  }
+      },
+      wednesday: {
+        name: 'Wednesday',
+        slots: {
+          900: {
+            time: '9:00am',
+            booked: false
+          },
+          0110: {
+            time: '11:00am',
+            booked: false
+          },
+          100: {
+            time: '1:00pm',
+            booked: false
+          },
+          300: {
+            time: '3:00pm',
+            booked: false
+          },
+          500: {
+            time: '5:00pm',
+            booked: false
+          },
+          700: {
+            time: '7:00pm',
+            booked: false
+          }
+    	  }
       }
     });
   };
